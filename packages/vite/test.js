227a5762a62f9a0c82ec902229ffbf309984fe91
Ryan Christian <ryanchristian4427@gmail.com>
2021-12-17 04:05:19 -0600

    test: Adding HTML tests to Vite, refactored test suites

--- packages/vite/test.js
+++ packages/vite/test.js
@@ -1,53 +1,80 @@
-import { test } from 'uvu';
+import { suite } from 'uvu';
 import * as assert from 'uvu/assert';
 
-import groupingPlugin from './dist/plugin.js';
+import groupingPlugin from './index.js';
 
 const plugin = groupingPlugin({ compress: true });
 
-test('type', () => {
+const form = suite('form');
+
+form('type', () => {
     assert.type(groupingPlugin, 'function');
 });
 
-test('instance', () => {
+form('instance', () => {
     assert.type(plugin, 'object');
     assert.type(plugin.transform, 'function');
 });
 
-const code = '<h1 class="text(blue-500 2xl)">Hello World</h1>';
-const id = 'foo.jsx';
-const transformed = { code: '<h1 class="text-blue-500 text-2xl">Hello World</h1>', map: null };
+form.run();
 
-test('Basic test of transform', async () => {
-    const result = await plugin.transform(code, id);
+// ---
+
+const jsx = suite('jsx');
+
+jsx('Basic test of transform', async () => {
+    const input = '<h1 class="text(blue-500 2xl)">Hello World</h1>';
+    const result = await plugin.transform(input, 'foo.jsx');
     assert.equal(result, transformed);
 });
 
-test('Early return if there is no JSX', async () => {
-    const result = await plugin.transform('console.log("foo")', id);
+jsx('Early return if there is no JSX', async () => {
+    const input = 'console.log("foo")';
+    const result = await plugin.transform(input, 'foo.jsx');
     assert.equal(result, undefined);
 });
 
 /**
- * @param {{ code: string, id: string, expect?: { code: string, map: null } }[]} data
+ * @param {{ id: string, expect?: { code: string, map: null } }[]} data
  */
 async function each(data, setup) {
     await Promise.all(data.map(setup));
 }
 
+const transformed = { code: '<h1 class="text-blue-500 text-2xl">Hello World</h1>', map: null };
+
 each(
     [
-        { code, id: 'foo.js' },
-        { code, id: 'foo.ts' },
-        { code, id: 'foo.jsx', expect: transformed },
-        { code, id: 'foo.tsx', expect: transformed },
+        { id: 'foo.js' },
+        { id: 'foo.ts' },
+        { id: 'foo.jsx', expect: transformed },
+        { id: 'foo.tsx', expect: transformed },
     ],
-    ({ code, id, expect }) => {
-        test(`Runs against correct IDs: ${id}`, async () => {
-            const result = await plugin.transform(code, id);
+    ({ id, expect }) => {
+        jsx(`Runs against correct IDs: ${id}`, async () => {
+            const input = '<h1 class="text(blue-500 2xl)">Hello World</h1>';
+            const result = await plugin.transform(input, id);
             assert.equal(result, expect);
         });
     },
 );
 
-test.run();
+jsx.run();
+
+// ---
+
+const html = suite('html');
+
+html('Avoid rewriting unnecessarily', async () => {
+    const input = '<h1 class="text-blue-500 text-2xl">Hello World</h1>';
+    const result = await plugin.transform(input, 'index.html');
+    assert.type(result, 'undefined');
+});
+
+html('Rewrites basic group', async () => {
+    const input = '<h1 class="text(blue-500 2xl)">Hello World</h1>';
+    const result = await plugin.transform(input, 'index.html');
+    assert.is(result.code, '<h1 class="text-blue-500 text-2xl">Hello World</h1>');
+});
+
+html.run();
