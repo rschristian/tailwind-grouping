ee56e3161f5fec7677bfc5890bf94fc7f223bc13
Ryan Christian <ryanchristian4427@gmail.com>
2021-12-17 04:04:54 -0600

    feat: Adding HTML processing to Vite plugin

--- packages/vite/index.js
+++ packages/vite/index.js
@@ -1,12 +1,23 @@
 import { parse, transform } from 'zecorn';
 
-import groupingPlugin from '../babel/index.js';
+import groupingPlugin, { process } from '../babel/index.js';
 
 export default function tailwindGroupingPlugin(options) {
     return {
         name: 'tailwind-grouping',
         enforce: 'pre',
         async transform(code, id) {
+            if (/\.html?$/.test(id)) {
+                const original = code;
+                const matches = code.matchAll(/class="([^"]*)/g);
+                for (const match of matches) {
+                    if (!match[1].includes('(')) continue;
+                    code = code.replace(match[1], process(match[1]));
+                }
+                if (original === code) return;
+                return { code };
+            }
+
             // Skip non-JSX/TSX files and files that don't contain JSX
             if (!/\.[jt]sx$/.test(id) || !/<[a-zA-Z$_][\w.:-]*[^>]*>/.test(code)) return;
 
